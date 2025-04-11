f0ceef39aa6e4ebff1ddf81d60da20412aa0e63e
Roman Vasilevskiy <roman.vasilevsk@gmail.com>
2025-04-11 15:37:37 +0300

    templates, forms, debugtoobar, etc

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_debugtoolbar import DebugToolbarExtension
+
 app = Flask(__name__)
-from app import routes
+app.config.from_object(Config)
+toolbar = DebugToolbarExtension(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models # noqa: E402
